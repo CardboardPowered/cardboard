5598512c5c4c4aa97b638b29fd9294f2279eb414
IsaiahPatton <isaiahpatton1@gmail.com>
2020-06-14 18:50:52 -0400

    Add /fabricmods

--- src/main/java/org/bukkit/craftbukkit/command/CraftCommandMap.java
+++ src/main/java/org/bukkit/craftbukkit/command/CraftCommandMap.java
@@ -5,6 +5,9 @@ import java.util.Map;
 import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.SimpleCommandMap;
+
+import com.fungus_soft.bukkitfabric.command.ModListCommand;
+//import com.fungus_soft.bukkitfabric.command.PluginsCommand;
 import com.fungus_soft.bukkitfabric.command.VersionCommand;
 
 public class CraftCommandMap extends SimpleCommandMap {
@@ -12,9 +15,10 @@ public class CraftCommandMap extends SimpleCommandMap {
     public CraftCommandMap(Server server) {
         super(server);
 
-        // Register our own custom version command
-        register("bukkit", new VersionCommand("version"));
-        register("bukkit", new VersionCommand("ver"));
+        // Register our commands
+        for (String s : new String[] {"version", "ver", "about"})
+            register("bukkit", new VersionCommand(s));
+        register("bukkit", new ModListCommand("fabricmods"));
     }
 
     public Map<String, Command> getKnownCommands() {
