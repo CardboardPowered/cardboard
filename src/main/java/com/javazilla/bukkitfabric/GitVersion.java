28859a53a9474bec1c93d47c949e4dac2f627200
IsaiahPatton <isaiahpatton1@gmail.com>
2020-09-01 19:59:11 -0400

    Include SQLite in Jar

--- src/main/java/com/javazilla/bukkitfabric/GitVersion.java
+++ src/main/java/com/javazilla/bukkitfabric/GitVersion.java
@@ -8,9 +8,9 @@ public class GitVersion {
 	public static final String MAVEN_GROUP = "com.fungus_soft";
 	public static final String MAVEN_NAME = "BukkitFabric";
 	public static final String VERSION = "1.16.2";
-	public static final int GIT_REVISION = 95;
-	public static final String GIT_SHA = "5d5d0e384adc7da30684664b6a02889857e56d57";
-	public static final String GIT_DATE = "2020-08-29T23:06:33Z";
-	public static final String BUILD_DATE = "2020-08-29T23:36:33Z";
-	public static final long BUILD_UNIX_TIME = 1598744193964L;
+	public static final int GIT_REVISION = 101;
+	public static final String GIT_SHA = "120abdbf717caca0f2dbd56f1af5ca0de449ec82";
+	public static final String GIT_DATE = "2020-09-01T21:39:15Z";
+	public static final String BUILD_DATE = "2020-09-01T23:54:04Z";
+	public static final long BUILD_UNIX_TIME = 1599004444544L;
 }
