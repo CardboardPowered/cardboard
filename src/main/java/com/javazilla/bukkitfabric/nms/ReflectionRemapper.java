f1739f68219a16218fd8e4a44dc2149678d2ebac
IsaiahPatton <isaiahpatton1@gmail.com>
2020-10-09 11:44:09 -0400

    Fix Linkage error with SpecialSource

--- src/main/java/com/javazilla/bukkitfabric/nms/ReflectionRemapper.java
+++ src/main/java/com/javazilla/bukkitfabric/nms/ReflectionRemapper.java
@@ -25,7 +25,6 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.SimplePluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
