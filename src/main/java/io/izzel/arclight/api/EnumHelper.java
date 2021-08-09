aebbc9d7d19dbe86de22b21924bf286cbb17d7b4
1a2s3d4f1 <40121560+1a2s3d4f1@users.noreply.github.com>
2021-08-10 00:12:53 +0800

    Fix OpenJ9 crash problem on 1.17
    This pull can fix https://github.com/CardboardPowered/cardboard/issues/232.

--- src/main/java/io/izzel/arclight/api/EnumHelper.java
+++ src/main/java/io/izzel/arclight/api/EnumHelper.java
@@ -84,8 +84,17 @@ public class EnumHelper {
 
     private static long enumConstantDirectoryOffset;
     private static long enumConstantsOffset;
+    private static long enumVarsOffset;
 
     static {
+        if (System.getProperty("java.vm.name").contains("OpenJ9")) {
+        try {
+              Field enumVars = Class.class.getDeclaredField("enumVars");
+              enumVarsOffset = Unsafe.objectFieldOffset(enumVars);
+          } catch (NoSuchFieldException e) {
+              throw new IllegalStateException(e);
+          }
+       } else {
         try {
             Field enumConstantDirectory = Class.class.getDeclaredField("enumConstantDirectory");
             Field enumConstants = Class.class.getDeclaredField("enumConstants");
@@ -96,10 +105,13 @@ public class EnumHelper {
             throw new IllegalStateException(e);
         }
     }
-
+}
     private static void reset(Class<?> cl) {
+        if (System.getProperty("java.vm.name").contains("OpenJ9")) {
+        Unsafe.putObject(cl, enumVarsOffset, null);
+        } else {
         Unsafe.putObject(cl, enumConstantDirectoryOffset, null);
         Unsafe.putObject(cl, enumConstantsOffset, null);
     }
-
+  }
 }
